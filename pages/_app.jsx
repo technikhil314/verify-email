5771e471dd2a67e86b91f764d5e645df05810dc1
Nikhil Mehta <nikhil.001mehta@gmail.com>
2021-09-12 21:01:29 +0530

    chore: removed google SEO file and added verification meta tag

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,18 @@
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="y76XmjuKHC8FEC0Mzf7SbK1235K4KzHHAHOVZfvLXwk"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
